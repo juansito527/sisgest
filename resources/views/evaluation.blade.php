f9ca1a570322f34f748d58e2a6525bf2a814d218
juansito527 <patousa1292@gmail.com>
2021-10-13 23:17:49 -0500

    se crea parte de preguntas

--- resources/views/evaluation.blade.php
+++ resources/views/evaluation.blade.php
@@ -16,50 +16,104 @@ class=" w-full z-30 top-0 text-white bg-white"
     <div class="col-span-5 m-auto  ">
         <div class="w-10/12 m-auto">
             <h1 class="text-6xl font-bold">Test evaluativo</h1>
-            <p class="text-justify mt-5">A continuacion resolveras el siguiente test teniendo en cuenta cada uno de los aspectos vistos en la empesa  "nombre empresa" 
+            <p class="text-justify mt-5">A continuacion resolveras el siguiente test teniendo en cuenta cada uno de los
+                aspectos vistos en la empesa "nombre empresa"
                 porfavor responder cada una de las preguntas
             </p>
         </div>
     </div>
-    
+
     {{-- fin titulo e imagen --}}
 
     {{-- TODO EL CUESTIONARIO --}}
     <div class="col-span-6 col-start-2 col-end-8 mt-5 m-auto w-full text-center shadow-2xl" id="scPlaneacion">
         {{-- tiulo antes del cuestionario --}}
         <label class="border-b text-4xl font-bold">RESOLUCIÓN 0312 DE 2019</label>
-        <p class="my-2 font-bold ">ESTÁNDARES MÍNIMOS DEL SISTEMA DE GESTIÓN DE LA SEGURIDAD Y SALUD PARA EMPLEADORES Y CONTRATANTES.</p>
+        <p class="my-2 font-bold ">ESTÁNDARES MÍNIMOS DEL SISTEMA DE GESTIÓN DE LA SEGURIDAD Y SALUD PARA EMPLEADORES Y
+            CONTRATANTES.</p>
         <div class="bg-white  text-black mx-5 border-2 border-solid border-white rounded mt-5">
             <p class="mt-5"><label class="font-bold  text-2xl text-indigo-600 plTitle">I. PLANEAR</label></p><br>
             <label class="font-medium italic">RECURSOS <label class="text-indigo-600">(10%)</label> </label>
-            <p class="font-medium text-1xl mt-2 p-2">Recursos financieros, técnicos humanos y de otra índole requeridos para coordinar y desarrollar el Sistema de Gestion de la Seguridad y Salud en el Trabajo (SG-SST) (4 %)</p>
+            <p class="font-medium text-1xl mt-2 p-2">Recursos financieros, técnicos humanos y de otra índole requeridos
+                para coordinar y desarrollar el Sistema de Gestion de la Seguridad y Salud en el Trabajo (SG-SST) (4 %)
+            </p>
         </div>
         {{-- FIN tiulo antes del cuestionario --}}
 
         {{-- FORMULARIO --}}
         <form action="">
-        <div class="grid grid-cols-4 bg-white  text-black mx-5 border border-solid border-gray-400 rounded shadow-2xl">
-            <div class="col-span-1">
-                <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Numeral</label></p>
-                <p>1.1.1</p>
+            <div
+                class="grid grid-cols-4 bg-white  text-black mx-5 border border-solid border-gray-400 rounded shadow-2xl pr-5">
+                <div class="col-span-1">
+                    <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Numeral</label></p>
+                    <p>1.1.1</p>
+                </div>
+                <div class="col-span-1">
+                    <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Item</label></p>
+                    <p class="text-justify mx-4">Asignacion de una persona que diseñe e implemente el Sistema de Gestion
+                        de SST</p>
+                </div>
+                <div class="col-span-1">
+                    <p class="mt-4 "><label class="font-bold text-justify text-indigo-600">Criterio</label></p>
+                    <p class="text-justify mx-4">Asignar una persona que cumpla conel siguiente perfil:
+                        El diseño e implementacion del Sistema de Gestion de SST Podra ser realizado
+                        por profesionales en SST, profesionales con posgrado en SST que cuenten con licencia en
+                        Seguridad y Salud en el Trabajo vigente y el curso de capacitacion virtual de cincuenta (50) horas.</p>
+                </div>
+                <div class="col-span-1">
+                    <p class="mt-4"><label class="font-bold text-justify text-indigo-600 ">Modo de verificación</label>
+                    </p>
+                    <p class="text-justify mx-4">Solicitar el documento en el que consta la asignacion, con la
+                        respectiva
+                        determinacion de responsabilidaes y constatar la hoja de vida con soportes de la persona
+                        asignada.</p>
+                </div>
+                <div class="grid grid-cols-3 col-span-4 bg-white  text-black mx-5 border  rounded shadow-2xl">
+                    <div class="col-span-1 w-44 text-left">
+                        <p class="mt-4 text-center"><label class="font-bold text-center text-indigo-600">Selecciona porfavor la
+                                calificacion</label></p>
+                        <input type="radio" class="ml-4 inline-block"> <p class="inline-block">Cumple Totalmente</p> <label class="text-red-300 ml-6">(0.05%)</label><br>
+                        <input type="radio" class="m-2 ml-4">No cumple<label class="text-red-300">(0.5%)</label><br>
+                        <input type="radio" class="m-2 ml-4">No aplica<br>
+                    </div>
+                    <div class="col-span-1 w-40">
+                        <p class="mt-4"><label
+                                class="font-bold text-justify text-indigo-600">Evidencias/Observaciones</label></p>
+                                <input type="file" class="text-1xl">
+                        <textarea name="" id="" cols="20" rows="5"
+                            class="border border-gray-400 m-2 rounded text-justify">
+                            
+                        </textarea>
+                        
+                    </div>
+                    <div class="col-span-1 w-40">
+                        <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Plan de Acción
+                                (Actividades)</label></p>
+                        <textarea name="" id="" cols="20" rows="5"
+                            class="border border-gray-400 m-2 rounded text-justify">
+                        </textarea>
+                    </div>
+                    <div class="col-span-1 w-40">
+                        <p class="mt-4"><label class="font-bold text-justify text-indigo-600"> Fecha (Plazo de
+                                Cumplimiento)</label></p>
+                        <input type="date" class="text-center m-2">
+                    </div>
+                    <div class="col-span-1 w-40">
+                        <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Recursos
+                            (Administrativos y Financieros)</label></p>
+                        <textarea name="" id="" cols="20" rows="5"
+                            class="border border-gray-400 m-2 rounded text-justify">
+                        </textarea>
+                    </div>
+                    <div class="col-span-1 w-40">
+                        <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Fundamentos y soportes de la efectividad de las acciones y actividades</label></p>
+                        <textarea name="" id="" cols="20" rows="5"
+                            class="border border-gray-400 m-2 rounded text-justify">
+                        </textarea>
+                    </div>
+                   
+                </div>
             </div>
-            <div class="col-span-1">
-                <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Item</label></p>
-                <p class="text-justify mx-4">Asignacion de una persona que diseñe e implemente el Sistema de Gestion de SST</p>
-            </div>
-            <div class="col-span-1">
-                <p class="mt-4 "><label class="font-bold text-justify text-indigo-600">Criterio</label></p>
-                <p class="text-justify mx-4">Asignar una persona que cumpla conel siguiente perfil:
-                    El diseño e implementacion del Sistema de Gestion de SST Podra ser realizado 
-                    por profesionales en SST, profesionales con posgrado en SST que cuenten con licencia en Seguridad 
-                    y Salud en el Trabajo vigente y el curso de capacitacion virtual de cincuenta (50) horas.</p>
-            </div>
-            <div class="col-span-1">
-                <p class="mt-4"><label class="font-bold text-justify text-indigo-600">Modo de verificación</label></p>
-                <p class="text-justify mx-4">Solicitar el documento en el que consta la asignacion, con la respectiva 
-                determinacion de responsabilidaes y constatar la hoja de vida con soportes de la persona asignada.</p>
-            </div>
-        </div>
         </form>
     </div>
 
